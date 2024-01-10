771ea76d3df2bd7f5d2add21dd94ebbcdfc3f0fe
Donovanmar <93877893+Donovanmar@users.noreply.github.com>
2024-01-10 16:21:08 +0000

    Modificación de contenidos ya existentes en el repositorio

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
   </head>
   <body>
         <?php 
-                echo '<h1>¡Hola Mundo! Donovan</h1>'
+                echo '<h1>¡Hola Mundo! Donovan</h1>';
+		echo '<p>Este archivo se encuentra establecido desde el repositorio <a href="https://github.com/Donovanmar/pps0-1Donovan">pps0-1Donovan</a></p>';
 	?>
   </body>
 </html>
