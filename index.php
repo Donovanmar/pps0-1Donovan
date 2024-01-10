0852d5bcfffabd06802f9dc8a14848eb0fe5e3f2
Donovanmar <donovanmar30@gmail.com>
2024-01-10 15:57:34 +0000

    Inserción de nuevos contenidos en el repositorio

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+        <title>Hola Mundo con PHP</title>
+  </head>
+  <body>
+        <?php 
+                echo '<h1>¡Hola Mundo! Donovan</h1>'
+	?>
+  </body>
+</html>
